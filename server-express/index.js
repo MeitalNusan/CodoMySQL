6c2c7f63c18cf32b9fa4b7a85d36dd93391e2a7b
Meital <meiti2002@hotmail.com>
2024-04-12 21:19:21 -0300

    comit2

--- server-express/index.js
+++ server-express/index.js
@@ -1,8 +1,14 @@
 import express from "express" 
+import cors from "cors"
 
 const app = express()
 
 const port = 3030
+
+app.use(cors())
+app.use(express.json)
+
+
 
 app.get("/", (req, res)=>{
     res.send("estas ok en el servidor")
