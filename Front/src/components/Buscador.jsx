7b05198fa47144be8feeb658349dfbc0bdaeeb98
Meital <meiti2002@hotmail.com>
2024-05-16 20:50:18 -0300

    buscador2

--- Front/src/components/Buscador.jsx
+++ Front/src/components/Buscador.jsx
@@ -1,12 +1,22 @@
 import { FaSearch } from "react-icons/fa";
 import "./buscador.css"
+import { useState } from "react";
 
 
 export const Buscador = () =>{
+    const [txtBuscador, setTxtBuscador] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+    }
+
     return(
-        <form className="containerBuscador">
+        <form className="containerBuscador" onSubmit={handleSubmit}>
             <div className="cajaBuscador">
                 <input 
+                value={txtBuscador}
+                onChange={(e)=>setTxtBuscador(e.target.value.toUpperCase())}
                 type="text"
                 className="inputBuscador"
                 />
