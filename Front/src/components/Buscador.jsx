4b527c0a47aeba2a757437537a3ab0f388822983
MeitalNusan <meiti2002@hotmail.com>
2024-05-18 13:02:21 -0300

    commit

--- Front/src/components/Buscador.jsx
+++ Front/src/components/Buscador.jsx
@@ -1,16 +1,19 @@
 import { FaSearch } from "react-icons/fa";
 import "./buscador.css"
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 
 export const Buscador = () =>{
     const [txtBuscador, setTxtBuscador] = useState("")
+    const navigate = useNavigate()
 
     const handleSubmit = (e) => {
         e.preventDefault()
-
+        navigate(`/?search=${txtBuscador}`)
     }
-
+    
     return(
         <form className="containerBuscador" onSubmit={handleSubmit}>
             <div className="cajaBuscador">
@@ -23,5 +26,7 @@ export const Buscador = () =>{
                 <button type="submit" className="botonBuscador"><FaSearch /></button>
             </div>
         </form>
+         
     )
+    
 }
