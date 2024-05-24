a07ebad45bba94dd8f1292d7db4838a783136869
Meital <meiti2002@hotmail.com>
2024-05-23 21:16:29 -0300

    buscador casi finalizado

--- Front/src/components/Show.jsx
+++ Front/src/components/Show.jsx
@@ -4,7 +4,115 @@ import { Link, useLocation } from "react-router-dom";
 import Swal from "sweetalert2"
 import { Spinner } from "./Spinner"
 import { Buscador } from "./Buscador";
- 
+  
+
+const Show = () => {
+    const [posts, setPosts] = useState([])
+    const [cargando, setCargando] = useState(true)
+
+      // caputrar url
+    const useQuery = () =>{
+        const location = useLocation()
+        return new URLSearchParams(useLocation().search)
+    }
+
+    const query = useQuery()
+    const search = query.get("search")
+    
+    
+    const getAllPost = async () => {
+        const searchURL = search? `https://rickandmortyapi.com/api/character/?name=` + search:'https://rickandmortyapi.com/api/character/';
+
+        try {
+            const res = await axios.get(searchURL);
+            setPosts(res.data.results);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        } finally {
+            setCargando(false);
+        }
+    }
+
+
+    const deletePost = async(id)=>{
+        await axios.delete(`https://rickandmortyapi.com/api/character/${id}`)
+        getAllPost()
+    }
+
+    const confirmarDelete = (id) =>{
+        Swal.fire({
+            title: "Estas seguro?",
+            text: "No se podrá revertir",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+                deletePost(id)
+              Swal.fire({
+                title: "Eliminado!",
+                text: "Tu documento ha sido eliminado",
+                icon: "success",
+              });
+            }
+          });
+        } 
+    
+
+    useEffect(()=>{
+        setCargando(true);
+         getAllPost()
+     },[search])
+
+    if(cargando){
+        return <Spinner/>
+    }
+    return(
+       <div className="container">
+        <div className="row">
+        <Buscador/>
+        <small>Create Post </small>
+            <div className="col">     
+                <Link to="/create" className="btn btn-primary mt-2">
+                  <i className="fa-regular fa-plus"></i>     
+                </Link>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Title</th> 
+                            <th>Content</th> 
+                            <th>Actions</th> 
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {posts.map((post=>(           
+                            <tr key={post.id}>
+                                <td>{post.name}</td>
+                                <td><img src={post.image} alt="" /></td>
+                                {/* <td>{post.content}</td> */}
+                                <td>
+                                    <Link to={`edit/${post.id}`} className="btn btn-primary">
+                                        <i className="fa fa-edit"></i>
+                                    </Link>
+                                    <button className="btn btn-danger" onClick={() => confirmarDelete(post.id)}>
+                                         <i className="fa-solid fa-trash"></i>
+                                    </button>
+                                </td>
+                            </tr>       
+                        )))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+       </div>
+    ) 
+    
+}
+
+export default Show;
+
 //no use useQuery para los errores
 
 
@@ -99,117 +207,3 @@ import { Buscador } from "./Buscador";
 
 // export default Show;
 
-
-const Show = () => {
-    const [posts, setPosts] = useState([])
-    const [cargando, setCargando] = useState(true)
-
-      // caputrar url
-      const useQuerys = () =>{
-        const location = useLocation()
-
-        return new URLSearchParams(useLocation().search)
-    }
-
-    const query = useQuerys()
-    const location = useLocation()
-
-
-    const search = query.get("search")
-    
-    
-    const getAllPost = async () => {
-        const searchURL = search
-            ? `https://rickandmortyapi.com/api/character/?name=`
-            : 'https://rickandmortyapi.com/api/character/';
-
-        try {
-            const res = await axios.get(searchURL);
-            setPosts(res.data.results);
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        } finally {
-            setCargando(false);
-        }
-    }
-
-
-    const deletePost = async(id)=>{
-        await axios.delete(`https://rickandmortyapi.com/api/character/${id}`)
-        getAllPost()
-    }
-
-    const confirmarDelete = (id) =>{
-        Swal.fire({
-            title: "Estas seguro?",
-            text: "No se podrá revertir",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Yes, delete it!"
-          }).then((result) => {
-            if (result.isConfirmed) {
-                deletePost(id)
-              Swal.fire({
-                title: "Eliminado!",
-                text: "Tu documento ha sido eliminado",
-                icon: "success",
-              });
-            }
-          });
-        } 
-    
-
-    useEffect(()=>{
-        setCargando(true);
-
-         getAllPost()
-     },[search])
-
-    if(cargando){
-        return <Spinner/>
-    }
-    return(
-       <div className="container">
-        <div className="row">
-        <Buscador/>
-        <small>Create Post </small>
-            <div className="col">     
-                <Link to="/create" className="btn btn-primary mt-2">
-                  <i className="fa-regular fa-plus"></i>     
-                </Link>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>Title</th> 
-                            <th>Content</th> 
-                            <th>Actions</th> 
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {posts.map((post=>(           
-                            <tr key={post.id}>
-                                <td>{post.name}</td>
-                                <td><img src={post.image} alt="" /></td>
-                                {/* <td>{post.content}</td> */}
-                                <td>
-                                    <Link to={`edit/${post.id}`} className="btn btn-primary">
-                                        <i className="fa fa-edit"></i>
-                                    </Link>
-                                    <button className="btn btn-danger" onClick={() => confirmarDelete(post.id)}>
-                                         <i className="fa-solid fa-trash"></i>
-                                    </button>
-                                </td>
-                            </tr>       
-                        )))}
-                    </tbody>
-                </table>
-            </div>
-        </div>
-       </div>
-    ) 
-    
-}
-
-export default Show;
